2d49a84b993644fdeb598b864857b9c6ee263d47
kristen-descant <kristendescant01@gmail.com>
2023-08-09 13:54:40 -0400

    secret key added to .env

--- reinvestmentpro/settings.py
+++ reinvestmentpro/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j*2_zok3q$fr668k1-az6i970k6tvhx0-%eu#%e3*g*0s)7$z'
+from dotenv import dotenv_values
+env = dotenv_values(".env")
+SECRET_KEY = env.get("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
