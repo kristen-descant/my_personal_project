0e52efa1e715d6b44c6311d40dfc9bc835b36c6f
kristen-descant <kristendescant01@gmail.com>
2023-08-11 18:46:10 -0400

    rent_api successful

--- reinvestmentpro/urls.py
+++ reinvestmentpro/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('user_app.urls')),
     path('api/properties/', include('property_app.urls')),
+    path('api/rent/', include('rent_api_app.urls')),
 ]
