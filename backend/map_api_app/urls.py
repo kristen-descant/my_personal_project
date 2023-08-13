ba7bc241b95466dfedad93929eec8464b0d6817d
kristen-descant <kristendescant01@gmail.com>
2023-08-13 15:35:01 -0400

    fixed apikey

--- backend/map_api_app/urls.py
+++ backend/map_api_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import Maps, Address_Autocomplete
 
 urlpatterns = [
-    path('<int:propid>', Maps.as_view(), name='rent'),
     path('autocomplete/', Address_Autocomplete.as_view(), name='address_autocomplete'),
 ]
--- backend/rent_api_app/urls.py
+++ backend/rent_api_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Rent
 
 urlpatterns = [
-    path('<int:propid>', Rent.as_view(), name='rent'),
+    path('<int:propid>/', Rent.as_view(), name='rent'),
 ]
